61d02c482eba3a9475e3b4ec868ebbd2ad6582e0
Michael M <m.meyer2k@gmail.com>
2015-06-20 03:22:16 -0500

    Update TemperTest.php

--- tests/TemperTest.php
+++ tests/TemperTest.php
@@ -5,7 +5,7 @@ class TemperTest extends PHPUnit_Framework_TestCase
 
     public function testFunctions()
     {
-        $temper = new Dopecode\Temper\Temper('1234');
+        $temper = new \Temper\Temper('1234');
         $this->assertTrue(strlen($temper->path()) > 0);
         $this->assertTrue($temper->destroy());
     }
